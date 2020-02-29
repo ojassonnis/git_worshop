2faef0c14561227179c9f6ad52aa72f71a3a8869
Manoj Bhandari <56146004+manoj95b@users.noreply.github.com>
2020-02-28 19:09:35 -0600

    Add files via upload

---
+++ workshop.py
@@ -0,0 +1,30 @@
+def add(a,b):
+    pass
+
+def subtract(a,b):
+    pass
+
+def multiply(a,b):
+    pass
+
+def divide(a,b):
+    pass
+
+def square(a):
+    pass
+
+def cube(x):
+    pass
+
+def remainder(a,b):
+    pass
+
+if __name__ == '__main__':
+    # A call to add function
+    add(1,2)
+    subtract(5,3)
+    multiply(2,4)
+    divide(44,2)
+    square(3)
+    cube(2)
+    remainder(9,7)
